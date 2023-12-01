152d4cb120d4b8b46286cad44a3fc2b56dfe6e0c
Hariharan M <hari@Hariharans-MacBook-Air.local>
2023-12-01 12:16:50 +0530

    cors issue

--- index.js
+++ index.js
@@ -3,8 +3,38 @@ const cors = require('cors')
 const app = express()
 const port = 3000
 const bodyParser = require('body-parser')
-app.options('*', cors())
+
+
+var allowCrossDomain = function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Cache-Control");
+
+  // intercept OPTIONS method
+  if ('OPTIONS' == req.method) {
+    res.send(200);
+  }
+  else {
+    next();
+  }
+};
+app.use(allowCrossDomain);
+
 app.use(cors());
+var allowCrossDomain = function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Cache-Control");
+
+  // intercept OPTIONS method
+  if ('OPTIONS' == req.method) {
+    res.send(200);
+  }
+  else {
+    next();
+  }
+};
+app.use(allowCrossDomain);
 app.use(bodyParser.json())
 
 require('./routes/index')(app)

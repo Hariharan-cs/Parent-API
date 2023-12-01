56167481dd670c12e520aef7dbbcda565e4bff6c
Hariharan M <hari@Hariharans-MacBook-Air.local>
2023-12-01 12:33:08 +0530

    cors issue

--- index.js
+++ index.js
@@ -4,37 +4,20 @@ const app = express()
 const port = 3000
 const bodyParser = require('body-parser')
 
+// app.use(cors());
+app.use(cors({
+  origin: true, // "true" will copy the domain of the request back
+                // to the reply. If you need more control than this
+                // use a function.
 
-var allowCrossDomain = function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Cache-Control");
+  credentials: true, // This MUST be "true" if your endpoint is
+                     // authenticated via either a session cookie
+                     // or Authorization header. Otherwise the
+                     // browser will block the response.
 
-  // intercept OPTIONS method
-  if ('OPTIONS' == req.method) {
-    res.send(200);
-  }
-  else {
-    next();
-  }
-};
-app.use(allowCrossDomain);
-
-app.use(cors());
-var allowCrossDomain = function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Cache-Control");
-
-  // intercept OPTIONS method
-  if ('OPTIONS' == req.method) {
-    res.send(200);
-  }
-  else {
-    next();
-  }
-};
-app.use(allowCrossDomain);
+  methods: 'POST,GET,PUT,OPTIONS,DELETE' // Make sure you're not blocking
+                                         // pre-flight OPTIONS requests
+}));
 app.use(bodyParser.json())
 
 require('./routes/index')(app)
@@ -42,3 +25,6 @@ require('./routes/index')(app)
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
+// localStorage.setItem('apiurl', 'https://auth-api.onedpo-poc.com');
+// localStorage.setItem('apiurl', 'http://localhost:3000');
